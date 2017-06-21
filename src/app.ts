5305532aa7ef61a99cef6a23a192250a2c0d0afa
jasofalcon <jasofalcon@gmail.com>
2017-06-22 01:54:35 +0200

    scss setup webpack

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+class Person {
+    constructor(private name: string, private age: number) {
+    }
+
+    public getName():string {
+        return this.name;
+    }
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+var path = require('path');
+
+module.exports = {
+    entry: ['./src/app.ts', './src/main.scss'],
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js', '.json', '.scss']
+    },
+
+    devtool: "eval",
+
+    module: {
+        loaders: [{
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+        }, {
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader',
+            exclude: /(node_modules)/
+        }, {
+            test: /\.html$/,
+            exclude: /node_modules/,
+            loader: 'raw'
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.template.ejs', inject: 'body' }),
+        new ExtractTextPlugin({ filename: "app.bundle.css", allChunks: true })
+    ]
+};
