f145cccc3c2e812e5bbdddccfd337b9d57faf3e2
jasofalcon <jasofalcon@gmail.com>
2017-08-21 16:58:21 +0200

    Controller created

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 const angular = require('angular');
+const ngRoute = require('angular-route');
+const AnimalsListController = require("./animalsList/animals-list.controller");
+const AnimalDetailsController = require("./animalDetails/animal-details.controller");
 
-const app = angular.module('app', []);
+
+angular.module('app', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "animalsList/animals-list.html",
+                controller: "AnimalsListController as vm"
+            })
+            .when("/details/:animal", {
+                templateUrl: "animalDetails/animal-details.html",
+                controller: "AnimalDetailsController as vm"
+            });
+    })
+    .controller("AnimalsListController", AnimalsListController)
+    .controller("AnimalDetailsController", AnimalDetailsController)
