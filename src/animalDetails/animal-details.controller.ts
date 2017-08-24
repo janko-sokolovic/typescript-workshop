ea144725cafeef255b6193268d1ddfe7f1a9c27c
jasofalcon <jasofalcon@gmail.com>
2017-08-24 16:22:28 +0200

    Routing added

---
+++ src/animalDetails/animal-details.controller.ts
@@ -0,0 +1,4 @@
+export default class AnimalDetailsController {
+  constructor() {
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,21 @@ import * as angular from "angular";
 import "angular-route";
 
 import AnimalsListController from "./animalsList/animalsList.controller";
+import AnimalDetailsController from "./animalDetails/animal-details.controller";
 
+let app: ng.IModule = angular.module("animals", ["ngRoute"])
 
-let app: ng.IModule = angular.module("animals", [])
+app.config(($routeProvider: ng.route.IRouteProvider) => {
+  $routeProvider
+      .when("/", {
+          template: require("./animalsList/animals-list.html"),
+          controller: "AnimalsListController as vm"
+      })
+      .when("/details/:animal", {
+          template: require("./animalDetails/animal-details.html"),
+          controller: "AnimalDetailsController as vm"
+      });
+});
 
-app.controller("AnimalsListController", AnimalsListController);
+app.controller("AnimalsListController", AnimalsListController)
+  .controller("AnimalDetailsController", AnimalDetailsController);
