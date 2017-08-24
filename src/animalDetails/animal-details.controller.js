b275aed9e79719022ba7c820059f58be05f0e12a
jasofalcon <jasofalcon@gmail.com>
2017-08-24 16:26:07 +0200

    Routing created

--- src/animalDetails/animal-details.controller.js
+++ src/animalDetails/animal-details.controller.js
@@ -1,5 +1,21 @@
-function AnimalDetialsController() {
+const ANIMALS = require('../animals.mock');
+
+function AnimalDetialsController($routeParams) {
     let vm = this;
+    if (!$routeParams || !isAnimal($routeParams.animal)) {
+        return;
+    }
+
+    vm.animal = ANIMALS.find(function(a){return a.name === $routeParams.animal})
+
+
+
+    function isAnimal(animalName) {
+        return ANIMALS.some(function(a) {
+            return a.name === animalName
+        });
+    }
+
 }
 
 module.exports = AnimalDetialsController;
